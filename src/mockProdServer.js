2ef07e8eceb046bd8440d540253de323b6c3cfce
windy <liuguang@kayak.com.cn>
2022-01-21 10:14:30 +0800

    2

--- src/mockProdServer.js
+++ src/mockProdServer.js
@@ -1,7 +1,6 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
 
 import user from '../mock/user'
-import product from '../mock/product'
 
 export function setupProdMockServer() {
     createProdMockServer([...user])
